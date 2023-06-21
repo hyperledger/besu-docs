884b24dcbc48bc7d75f645c3f6a200e4a1fa7316
Alexandra Tran <alexandra.tran@consensys.net>
2023-06-21 13:28:57 -0700

    clear jsx warnings
    Signed-off-by: Alexandra Tran <alexandra.tran@consensys.net>
    Signed-off-by: Joshua Fernandes <joshua.fernandes@consensys.net>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import clsx from "clsx";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageCards from "@site/src/components/HomepageCards";
 

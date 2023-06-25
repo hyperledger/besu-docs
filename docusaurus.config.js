33311cbff198f93885d6c519ab50a59bcc4e275e
Joshua Fernandes <joshua.fernandes@consensys.net>
2023-06-26 07:51:41 +1000

    update yarn.lock && add redirect for en/stable->stable
    Signed-off-by: Joshua Fernandes <joshua.fernandes@consensys.net>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -290,6 +290,9 @@ const config = {
               existingPath.replace("/development", "/latest"),
             ];
           }
+          if (existingPath.includes("/stable")) {
+            return [existingPath.replace("/stable", "/en/stable")];
+          }
           return undefined; // Return a falsy value: no redirect created
         },
       },

d61a42bc6108bee179699f4cbda0cd4b0a4a4d2a
garyschulte <garyschulte@gmail.com>
2023-12-30 17:07:49 -0800

    release docs 23.10.3 (#1474)
    Signed-off-by: garyschulte <garyschulte@gmail.com>
    Signed-off-by: Joshua Fernandes <joshua.fernandes@consensys.net>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           path: "./docs",
           includeCurrentVersion: true,
           // Set to the last stable release
-          lastVersion: "23.10.2",
+          lastVersion: "23.10.3",
           versions: {
             //defaults to the ./docs folder
             // using 'development' instead of 'next' as path
@@ -46,8 +46,11 @@ const config = {
               path: "development",
             },
             // The last stable release in the versioned_docs/version-stable
+            "23.10.3": {
+              label: "stable (23.10.3)",
+            },
             "23.10.2": {
-              label: "stable (23.10.2)",
+              label: "23.10.2",
             },
             "23.7.3": {
               label: "23.7.3",
---
+++ versioned_docs/version-23.10.3/private-networks/how-to/send-transactions/index.md
@@ -0,0 +1,17 @@
+---
+title: Create and send transactions
+description: private networks send transactions overview
+tags:
+  - private networks
+---
+
+# Create and send transactions
+
+In private networks, you can create and [send regular transactions](../../../public-networks/how-to/send-transactions.md) as in public networks.
+
+You can also:
+
+- [Send private transactions](private-transactions.md).
+- [Send concurrent private transactions](concurrent-private-transactions.md).
+- [Include revert reason in transactions](revert-reason.md).
+- [Replace transactions in the transaction pool](../../../public-networks/concepts/transactions/pool.md#in-networks-with-zero-base-base-or-free-gas).
